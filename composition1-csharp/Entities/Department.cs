157ba98e3633041af861d724c52116d8058c3281
Joao Rodrigo de Oliveira Ziegler <J1075@sollistico.de>
2021-02-26 08:02:19 +0100

    First commit

---
+++ composition1-csharp/Entities/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace composition1_csharp.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
